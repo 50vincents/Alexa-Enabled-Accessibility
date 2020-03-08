120945072e774b43ee366f4770366a4bdbd09149
50vincents <vincethai035@gmail.com>
2020-03-08 15:41:12 -0700

    dynamodb config

--- index.js
+++ index.js
@@ -5,13 +5,32 @@ const AWS = require('aws-sdk')
 const port = process.env.PORT || 3000;
 const app = express();
 
+const awsConfig = require("./config")
+
 AWS.config.update({
-    region: "us-west-2",
-    endpoint: "http://localhost:8000"
+  region: "us-east-1",
+  endpoint: "https://dynamodb.us-east-1.amazonaws.com",
+  accessKeyId: awsConfig.accessKey,
+  secretAccessKey: awsConfig.secretKey,
 });
 
-const dynamodb = new AWS.DynamoDB();
+const docClient = new AWS.DynamoDB.DocumentClient();
 
+const table = "frequencies";
+const params = {
+    TableName:table,
+    Item:{
+        "frequency_level": frequency,
+    }
+};
+
+docClient.put(params, function(err, data) {
+    if (err) {
+        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Added item:", JSON.stringify(data, null, 2));
+    }
+});
 
 
 const LaunchRequestHandler = {
